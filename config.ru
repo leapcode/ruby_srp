d0f10aab67ab679b17bfad76b242ecf7fc7177f6
Azul <azul@leap.se>
2012-06-26 17:33:57 +0200

    added simple sinatra app for demoing / testing

---
+++ config.ru
@@ -0,0 +1,5 @@
+require 'rubygems'
+require 'sinatra'
+require 'http-srp'
+
+run Sinatra::Application
---
+++ http-srp.rb
@@ -0,0 +1,47 @@
+require 'sinatra'
+require 'pp'
+
+class User
+
+  def self.current
+    # p "getting #{@current ? @current.login : 'nil'}"
+    @current ||= User.new
+  end
+
+  attr_accessor :login
+  attr_accessor :password
+  attr_accessor :active
+
+  def signup!(params)
+    self.login = params.delete('login')
+    p "signing up as #{login}"
+    self.password = params.delete('password')
+    self.active = false
+  end
+
+  def login!(params)
+    self.active = valid_login?(params[:login], params[:password])
+  end
+
+  def valid_login?(login, password)
+    (self.login == login) and (self.password == password)
+  end
+end
+
+get '/' do
+  @user = User.current
+  p "visiting / as #{@user.login}"
+  erb :index
+end
+
+post '/signup' do
+  @user = User.current
+  @user.signup!(params)
+  redirect '/'
+end
+
+post '/login' do
+  @user = User.current
+  @user.login!(params)
+  redirect '/'
+end
